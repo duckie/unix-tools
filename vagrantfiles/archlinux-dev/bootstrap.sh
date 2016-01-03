d7d06e452b528cbdbc19330e156a001a1603ce88
Jean-Bernard Jansen <jeanbernard.jansen@gmail.copm>
2016-01-04 00:22:40 +0100

    Add a vagrant default env for C++ dev

---
+++ vagrantfiles/archlinux-dev/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Add archfr repo
+has_archfr=$(grep -c archlinuxfr /etc/pacman.conf)
+if [[ 0 == ${has_archfr} ]]; then
+  cat << EOF >> /etc/pacman.conf
+
+[archlinuxfr]
+SigLevel = Never
+Server = http://repo.archlinux.fr/\$arch
+EOF
+fi
+
+# Install software
+pacman -Sy --noconfirm
+pacman -S --noconfirm --needed autoconf automake bash bash-completion binutils bison boost boost-libs bzip2 clang clang-analyzer clang-tools-extra cmake coreutils cracklib cryptsetup curl diffutils docker elfutils fakeroot file filesystem findutils flex gawk gc gcc gcc-libs gd gdb gdbm gettext git glib2 glibc gnupg gnutls grep groff gtest gzip htop iftop inetutils iproute2 iputils less libevent logrotate lsof lua lz4 lzo m4 make man-db man-pages ncurses net-tools netctl netstat-nat openresolv openssh openssl pacman pacman-mirrorlist pkg-config pkgfile procps-ng python python2 readline ruby sed strace sudo sysfsutils sysstat tar tig tmux tzdata valgrind vi vim vim-runtime which xz yaourt zlib zziplib
