3fcccd127a56f9d990f46790ffc7cac93447fd4e
Jean-Bernard Jansen <jeanbernard.jansen@gmail.copm>
2017-05-01 16:43:22 +0200

    Modify vagrantfile for dev archlinux

--- vagrantfiles/archlinux-dev/bootstrap.sh
+++ vagrantfiles/archlinux-dev/bootstrap.sh
@@ -12,5 +12,5 @@ EOF
 fi
 
 # Install software
-pacman -Sy --noconfirm
-pacman -S --noconfirm --needed autoconf automake bash bash-completion binutils bison boost boost-libs bzip2 clang clang-analyzer clang-tools-extra cmake coreutils cracklib cryptsetup curl diffutils docker elfutils fakeroot file filesystem findutils flex gawk gc gcc gcc-libs gd gdb gdbm gettext git glib2 glibc gnupg gnutls grep groff gtest gzip htop iftop inetutils iproute2 iputils less libevent logrotate lsof lua lz4 lzo m4 make man-db man-pages ncurses net-tools netctl netstat-nat openresolv openssh openssl pacman pacman-mirrorlist pkg-config pkgfile procps-ng python python2 readline ruby sed strace sudo sysfsutils sysstat tar tig tmux tzdata valgrind vi vim vim-runtime which xz yaourt zlib zziplib
+pacman -Suy --noconfirm
+pacman -S --noconfirm --needed autoconf automake bash bash-completion binutils bison boost boost-libs bzip2 clang clang-analyzer clang-tools-extra cmake coreutils cracklib cryptsetup curl diffutils docker elfutils fakeroot file filesystem findutils flex gawk gc gcc gcc-libs gd gdb gdbm gettext git glib2 glibc gnupg gnutls grep groff gtest gzip htop iftop inetutils iproute2 iputils less libevent logrotate lsof lua lz4 lzo m4 make man-db man-pages ncurses net-tools netctl netstat-nat openresolv openssh openssl pacman pacman-mirrorlist pkg-config pkgfile procps-ng python python2 readline ruby sed strace sudo sysfsutils sysstat tar tig tmux tzdata valgrind vi vim vim-runtime which xz yaourt zlib zziplib rsync go ninja
