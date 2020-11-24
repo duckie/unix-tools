07141be2aed5ea059260050434c7808a62946bdd
Jean-Bernard JANSEN <canard@merlin.localdomain>
2020-11-24 14:26:49 +0100

    Polybar conf for mobile variant

---
+++ config/polybar/variables.sh
@@ -0,0 +1,12 @@
+export POLYBAR_DPI="150"
+export POLYBAR_HEIGTH="3%"
+export POLYBAR_ETHERNET_INTERFACE="wlp1s0"
+export POLYBAR_WLAN_INTERFACE="wlp1s0"
+
+#export POLYBAR_MODULES_RIGHT="filesystem xbacklight pulseaudio xkeyboard memory cpu wlan battery temperature date powermenu"
+
+# Mobile variant
+export POLYBAR_MODULES_RIGHT="filesystem pulseaudio xkeyboard memory cpu wlan battery temperature date powermenu"
+
+# Workstation variant
+export POLYBAR_MODULES_RIGHT="filesystem pulseaudio memory cpu eth temperature date powermenu"
